03d40dfdd4dde433c844020d18ae060e5823fc75
Lucia Little <73139437+chufakiks@users.noreply.github.com>
2024-11-08 14:10:04 +0100

    Update CPUTop.scala

--- src/main/scala/CPUTop.scala
+++ src/main/scala/CPUTop.scala
@@ -1,6 +1,5 @@
 import chisel3._
 import chisel3.util._
-import firrtl.FirrtlProtos.Firrtl.Expression.Mux
 
 class CPUTop extends Module {
   val io = IO(new Bundle {
@@ -30,31 +29,60 @@ class CPUTop extends Module {
   val registerFile = Module(new RegisterFile())
   val controlUnit = Module(new ControlUnit())
   val alu = Module(new ALU())
+  /*
   val muxImmediate = Mux(controlUnit.io.memRead, registerFile.io.output2, registerFile.io.output1)
   val muxWriteReg = Mux(controlUnit.io.regWrite, alu.io.resultint,dataMemory.io.dataRead)
   val muxJumpSelector = Mux(controlUnit.io.jump, alu.io.resultbool, controlUnit.io.memRead)
   val muxJump = Mux(muxJumpSelector, programMemory.io.address, programCounter.io.programCounter)
+  */
+  
 
+  // spliting up the instruction
+  val instruction = programMemory.io.instructionRead
 
+  //muxes
 
+  //mux inbetween datamemory and register file
+  val muxdatareg = Mux(controlUnit.io.memRead, dataMemory.io.dataRead, alu.io.resultint)
+
+  //mux inbetween registerfile and alu
+  //val muxregalu = Mux(sel er ikke implerenteret, instruction(17,8),registerFile.io.readAdress2)
+
+  //mux for jump
+  //val muxcontrolunit = Mux(alu.io.resultbool,controlUnit.io.jump,what to put here)
+
+  //mux inbetween pc and adder
+  val muxpcadd = Mux(muxcontrolunit, programCounter.io.programCounter + 1, instruction(17,11))
 
   //Connecting the modules
 
+  //programcounter
   programCounter.io.run := io.run
+
+  //programmemory
   programMemory.io.address := programCounter.io.programCounter
+
+  //registerfile
+  registerFile.io.writeData := muxdatareg
+  registerFile.io.readAdress1 := instruction(22,18)
+  registerFile.io.readAdress2 := instruction(17,12)
+  registerFile.io.writeSel := instruction(27,23)
+  registerFile.io.writeEnable := controlUnit.io.regWrite
+
+  //alu
+  alu.io.A := registerFile.io.readAdress1
+  //alu.io.B := muxregalu
   alu.io.aluOp := controlUnit.io.aluOp
-  programCounter.io.stop := controlUnit.io.stop
-  registerFile.io.readAdress1 := programMemory.io.instructionRead
-  registerFile.io.readAdress2 := programMemory.io.instructionRead
-  registerFile.io.writeSel := programMemory.io.instructionRead
 
-  muxImmediate := programMemory.io.address
-  muxWriteReg := 
+  //control unit
+  controlUnit.io.opcode := instruction(31,28)
 
+  //data memory
+  dataMemory.io.address := controlUnit.io.aluOp
+  dataMemory.io.dataWrite := registerFile.io.writeData
+  dataMemory.io.writeEnable := controlUnit.io.memWrite
 
-  ////////////////////////////////////////////
-  //Continue here with your connections
-  ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
   //This signals are used by the tester for loading the program to the program memory, do not touch
   programMemory.io.testerAddress := io.testerProgMemAddress
