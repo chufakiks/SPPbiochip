0ace05e954a51762af26fef6a9721f698753d15b
Kajsapixar <kajsa-2001@hotmail.com>
2024-11-11 19:12:18 +0100

    Added val to controlU

--- src/main/scala/ControlUnit.scala
+++ src/main/scala/ControlUnit.scala
@@ -11,6 +11,11 @@ class ControlUnit extends Module {
     val jump = Output(Bool()) //jump or not
     val stop = Output(Bool())
     //Define the module interface here (inputs/outputs)
+    val aluSrc = Output(Bool())        // if ALU uses or immediate
+    val writeBackSel = Output(Bool())  // chooses datasource for Writeback
+    val jeq = Output(Bool())           // Specific signal for JEQ-instruktioner
+    val jlt = Output(Bool())           // Specific signal for JLT-instruktioner
+    val counterEnable = Output(Bool()) // controls Counter, if needed
   })
 
   switch(io.opcode) {
@@ -19,32 +24,38 @@ class ControlUnit extends Module {
       io.aluOp := 0.U //add op
       io.memRead := false.B
       io.memWrite := false.B
+      io.writeBackSel := true.B // writes back from ALU
     }
     is(1.U) { // SUB
       io.regWrite := true.B
       io.aluOp := 1.U // SUB operation
       io.memRead := false.B
       io.memWrite := false.B
+      io.writeBackSel := true.B
     }
     is(2.U) { // MULT
       io.regWrite := true.B
       io.aluOp := 2.U // MULT operation
       io.memRead := false.B
       io.memWrite := false.B
+      io.writeBackSel := true.B
     }
     is(3.U) { // ADDI
       io.regWrite := true.B
       io.aluOp := 3.U // ADDI operation
       io.memRead := false.B
       io.memWrite := false.B
+      io.aluSrc := true.B  // ALU uses immediate value as operator
+      io.writeBackSel := true.B
     }
     is(4.U) { // LI (Load Immediate)
       io.regWrite := true.B
       io.aluOp := 4.U // LI operation
       io.memRead := false.B
       io.memWrite := false.B
+      io.writeBackSel := true.B
     }
-    is(5.U) { //LD
+    is(5.U) { //LD (Load from Data Memory)
       io.regWrite := true.B
       io.memRead := true.B
       io.memWrite := false.B
@@ -66,16 +77,17 @@ class ControlUnit extends Module {
     is(8.U) { // JEQ
       io.jump := true.B
       io.aluOp := 1.U //SUB
+      io.jeq := true.B  // activates JEQ-signal
     }
     is(9.U) { // JLT
       io.jump := true.B
       io.aluOp := 1.U //sub
+      io.jlt := true.B  // activates JLT-signal
     }
     is(10.U) {
       io.stop := true.B
+      io.counterEnable := false.B
     }
   }
-
   //Implement this module here
-
 }
