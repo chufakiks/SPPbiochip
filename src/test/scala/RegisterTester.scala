1f9e7af764670fc9931b0e3e55265bc21c7761d8
Lucia Little <73139437+chufakiks@users.noreply.github.com>
2024-11-16 11:59:02 +0100

    Update RegisterTester.scala

--- src/test/scala/RegisterTester.scala
+++ src/test/scala/RegisterTester.scala
@@ -1,20 +1,21 @@
-/*import chisel3._
+import chisel3._
 import chisel3.iotesters.PeekPokeTester
 
 class RegisterTester(dut: RegisterFile) extends PeekPokeTester(dut) {
 
-  //Program Counter running for 5 clock cycles
-  poke(dut.io.readAdress1, 1)
+  //LD
+  poke(dut.io.readAdress1, 4)
   poke(dut.io.readAdress2, 0)
-  poke(dut.io.writeSel, 1)
+  poke(dut.io.writeSel, 5)
   poke(dut.io.writeData, 0)
-  poke(dut.io.writeEnable,1)
-  step(5)
-  System.out.print(peek(dut.io.))
-  System.out.print("\n")
+  poke(dut.io.writeEnable, true)
+  step(1)
+
+  poke(dut.io.readAdress1, 5)
+  System.out.println(peek(dut.io.output1))
 }
 
-object ProgramCounterTester {
+object RegisterTester {
   def main(args: Array[String]): Unit = {
     println("Testing RegisterFile")
     iotesters.Driver.execute(
@@ -26,4 +27,3 @@ object ProgramCounterTester {
     }
   }
 }
-*/
