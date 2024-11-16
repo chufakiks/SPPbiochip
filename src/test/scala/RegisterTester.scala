40fd29d3567aafb9ddd238ab3cce79b7cdfc29b0
Lucia Little <73139437+chufakiks@users.noreply.github.com>
2024-11-16 19:56:58 +0100

    🤡

--- src/test/scala/RegisterTester.scala
+++ src/test/scala/RegisterTester.scala
@@ -5,9 +5,9 @@ class RegisterTester(dut: RegisterFile) extends PeekPokeTester(dut) {
 
   //LD
   poke(dut.io.readAdress1, 4)
-  poke(dut.io.readAdress2, 0)
+  poke(dut.io.readAdress2, 5)
   poke(dut.io.writeSel, 5)
-  poke(dut.io.writeData, 0)
+  poke(dut.io.writeData, 200)
   poke(dut.io.writeEnable, true)
   step(1)
 
